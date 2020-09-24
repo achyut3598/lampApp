08b23eb1c62160a9b2d9a1e0793b93f0624a389b
Achyut <achyut3598@gmail.com>
2020-09-24 18:29:47 -0400

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -68,7 +68,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 		</table>
 	</div>
     <p>
-        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
         <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
     </p>
 </body>
